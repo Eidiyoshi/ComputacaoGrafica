cef09b9b9e1226489126d5b584a8e7558d4442d7
eidiyoshi <eidiyoshi@hotmail.com>
2025-09-23 18:54:37 -0300

    bresheman wip

--- backup/unit1.pas
+++ backup/unit1.pas
@@ -29,6 +29,7 @@ type
     MenuItem6: TMenuItem;
     MenuItem7: TMenuItem;
     MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
     procedure FormCreate(Sender: TObject);
     procedure Image1Click(Sender: TObject);
     procedure Image1MouseDown(Sender: TObject; Button: TMouseButton;
@@ -43,6 +44,7 @@ type
     procedure MenuItem6Click(Sender: TObject);
     procedure MenuItem7Click(Sender: TObject);
     procedure MenuItem8Click(Sender: TObject);
+    procedure MenuItem9Click(Sender: TObject);
   private
 
   public
@@ -54,8 +56,8 @@ var
   op: Integer;
   desenhar, linhar : Boolean;
   m,dx,dy: Float;
-  x1,y1,x2,y2,x,y,xc,yc,inc,i: Integer;
-  R,xr,yr,a,sen1,cos1,xn : Real;
+  x1,y1,x2,y2,x,y,xc,yc,inc,incx,incy,i: Integer;
+  R,xr,yr,a,sen1,cos1,xn,d,dE,dNE : Real;
 
 implementation
 
@@ -80,12 +82,14 @@ begin
   begin
        desenhar := true;
        end;
+
   if (op = 2) then
   begin                              
     Image1.Canvas.Pen.Color := clred;
     Image1.Canvas.MoveTo(X,Y);
   end;
-  if ((op = 3) and (linhar = false))  then // linhas manuais
+
+  if ( (op = 3) and (linhar = false))  then // linhas manuais
   begin
     x1 := X;
     Edit3.Text := IntToStr(x1);
@@ -93,6 +97,15 @@ begin
     Edit4.Text := IntToStr(y1);
     linhar := true;
   end;
+
+  if( op = 4 ) then   // linha bresenham
+  begin
+    x1 := X;
+    Edit3.Text := IntToStr(x1);
+    y1 := Y;
+    Edit4.Text := IntToStr(y1);
+  end;
+
   if ((op = 5) or (op = 6) or (op = 7)) then // circunferencia
   begin
        xc := X;
@@ -112,7 +125,9 @@ procedure TForm1.Image1MouseUp(Sender: TObject; Button: TMouseButton;
   Shift: TShiftState; X, Y: Integer);
 begin
   if (op = 1) then desenhar := false;
+
   if (op = 2) then Image1.Canvas.LineTo(X,Y);
+
   if ((op = 3) and (linhar = true)) then
   begin // desenhar linhas manuais
     x2 := X;
@@ -173,10 +188,79 @@ begin
     end;
     linhar := false;
     end;
+
   if(op = 4) then
   begin // desenhar linhas bresenham
-       x := X;
+
+        x2 := X;
+        y2 := Y;
+        Edit5.Text := IntToStr(x2);
+        Edit6.Text := IntToStr(y2);
+
+        dx := x2 - x1;
+        dy := y2 - y1;
+
+        d := 2 * dy - dx;
+        dE := 2 * dy;
+        dNE := 2 * ( dy - dx );
+        x := x1;
+        y := y1;
+        Image1.Canvas.Pixels[x,y] := clred;
+
+        incx := 1;
+        incy := 1;
+
+        if( dy < 0 ) then
+        begin
+        incy := -1;
+        end;
+
+        if( dx < 0 ) then
+        begin
+        incx := -1;
+        end;
+
+        if(abs(x2-x1) > abs(y2-y1)) then   // linha mais horizontal
+        begin
+          while( x <> x2 ) do
+          begin
+              if( d < 0 ) then
+                begin
+                  d := d + dE;
+                  x := x + incx;
+                end
+              else
+                begin
+                  d := d + dNE;
+                  x := x + incx;
+                  y := y + incy;
+                end;
+              Image1.Canvas.Pixels[x,y] := clred;
+          end;
+        end;
+
+        if(abs(y2-y1) >= abs(x2-x1)) then   // linha mais vertical
+        begin
+          while(y <> y2) do
+          begin
+              if( d < 0 ) then
+                begin
+                  d := d + dE;
+                  y := y + incy;
+                end
+              else
+                begin
+                  d := d + dNE;
+                  y := y + incy;
+                  x := x + incx;
+                end;
+              Image1.Canvas.Pixels[x,y] := clred;
+          end;
+        end;
+       incx := 1;
+       incy := 1;
   end;
+
   if(op = 5) then
   begin // circunferencia
        R := sqrt((xc-X)*(xc-X) + (yc-Y)*(yc-Y));
@@ -189,6 +273,7 @@ begin
             xr := xr + 0.01;
        end;
   end;
+
   if(op = 6) then
   begin // circunferencia parametrica                 
         R := sqrt((xc-X)*(xc-X) + (yc-Y)*(yc-Y));
@@ -201,6 +286,7 @@ begin
              a := a + 0.01;
         end;
   end;
+
   if(op = 7) then
   begin // circ sen1
         xr := sqrt((xc-X)*(xc-X) + (yc-Y)*(yc-Y));
@@ -211,7 +297,7 @@ begin
         begin
           xn := xr * cos1 - yr * sen1;
           yr := xr * sen1 + yr * cos1;
-          x := xn;
+          xr := xn;
           Image1.Canvas.Pixels[round(xc+xr),round(yc+yr)] := clred;
         end;
   end;
@@ -224,12 +310,17 @@ end;
 
 procedure TForm1.MenuItem4Click(Sender: TObject);
 begin
-  op := 2 // desenhar linha
+  op := 2; // desenhar linha
 end;
 
 procedure TForm1.MenuItem5Click(Sender: TObject);
 begin
-  op := 3 // desenhar linhas na "mão"
+  op := 3; // desenhar linhas na "mão"
+end;
+                   
+procedure TForm1.MenuItem9Click(Sender: TObject);
+begin
+  op := 4; //desenhar linha bresenham
 end;
 
 procedure TForm1.MenuItem6Click(Sender: TObject);
@@ -246,6 +337,7 @@ procedure TForm1.MenuItem8Click(Sender: TObject);
 begin
   op := 7;  // circ sin1
 end;
+
 
 end.
 
